3a186e9935bd82c6369f2bbd6479cced5daa9089
Samuel Gómez Gutiérrez <143805962+Sggz221@users.noreply.github.com>
2025-03-02 20:44:31 +0100

    Revert "Avance en Storage CSV y lectura"

--- src/main/kotlin/repositories/EquipoRepository.kt
+++
@@ -1,7 +0,0 @@
-package org.example.repositories
-
-import org.example.models.Integrante
-
-interface EquipoRepository<T, U> : CrudRepository<Long, Integrante> {
-    fun deleteLogical(id: Long, entity: Integrante): Integrante?
-}
--- src/main/kotlin/storage/EquipoStorage.kt
+++
@@ -1,9 +0,0 @@
-package org.example.storage
-
-import org.example.models.Integrante
-import java.io.File
-
-interface EquipoStorage {
-    fun fileRead(file: File, format: String): List<Integrante>
-    fun fileWrite(integrantes: List<Integrante>, file: File, format: String)
-}
