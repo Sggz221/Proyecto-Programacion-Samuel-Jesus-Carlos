9fab0618b0d44327dd0c9c0d4b771afe4ce0a916
charliee.cy <carloscy8@gmail.com>
2025-03-04 19:15:27 +0100

    Menú completo, faltan consultas

--- src/main/kotlin/view/Menu.kt
+++ src/main/kotlin/view/Menu.kt
@@ -6,10 +6,8 @@ import org.example.models.Jugador
 import org.example.models.Posicion
 import org.example.service.Service
 import org.example.service.ServiceImpl
+import java.io.File
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 
 class Menu (
@@ -45,22 +43,98 @@ class Menu (
         when(option){
             1 -> cargarDatos()
             2 -> crearMiembro()
+            3 -> actualizarMiembro()
+            4 -> borrarMiembro()
             5 -> service.getAll().forEach {println(it)}
+            6 -> exportarDatos()
+            7 -> {}
             8 -> {}
         }
     }
+
+    fun preguntarFormato(importar: Boolean): String{
+        var option: Int
+        do {
+            if (importar) println("¿De qué fichero importar los datos del equipo? \n1| CSV, 2| JSON, 3| XML, 4| BIN: ")
+            else println("En qué formato quiere exportar los datos del equipo? \n1| CSV, 2| JSON, 3| XML, 4| BIN: ")
+
+            option = readln().toIntOrNull() ?: 0
+            if (option !in 1..4) println("Opcion no valida.")
+        }while (option !in 1..4)
+
+        var extension: String
+        when(option){
+            1 -> extension = "csv"
+            2 -> extension = "json"
+            3 -> extension = "xml"
+            else -> extension = "bin"
+        }
+        return extension
+    }
+
+    fun preguntarNombre(): String {
+        val regEx = """^[a-zA-Z0-9_-]+$""".toRegex()
+        var nombre: String
+
+        do {
+            println("¿Qué nombre quiere ponerle al archivo?\nSolo caracteres alfanuméricos, guiones - y guiones bajos _")
+            nombre = readln()
+            if (!nombre.matches(regEx)) println("Nombre no válido")
+        } while(!nombre.matches(regEx))
+
+        return nombre
+    }
+
+    private fun exportarDatos() {
+        val extension = preguntarFormato(false)
+        val nombre = preguntarNombre()
+        val file = File("backup", "${nombre}.${extension}")
+        service.exportToFile(file.path)
+    }
+
+
+    fun actualizarMiembro() {
+        TODO()
+    }
+
+    fun borrarMiembro() {
+        val id = preguntarID()
+
+        try {
+            val jugador = service.getById(id)
+            service.deleteLogical(id,jugador)
+        } catch(e:Exception){
+            println(e.message)
+        }
+    }
+
+    fun preguntarID(): Long {
+        var input: Long?
+
+        do {
+            println("Escriba el id del jugador")
+            input = readln().toLongOrNull()
+            if (input == null) println("Dato no válido")
+        }while (input == null)
+
+        return input
+    }
+
+
     fun cargarDatos(){
-        println("Especifique la ruta del archivo del que desea importar los datos: ")
-        val path = readln()
+        val formato = preguntarFormato(true)
+        val file = File("data", "personal.${formato}")
+
         println("Leyendo fichero...")
         try {
-            service.importFromFile(path)
+            service.importFromFile(file.path)
         } catch (e: Exception) {
             println("Error al importar datos: ${e.message}")
         }
         println("Fichero cargado con exito")
     }
-    fun crearMiembro(){
+
+    fun preguntarRol (): String {
         var rol: String
         do{
             println("Quieres crear un jugador o un entrenador?")
@@ -68,22 +142,35 @@ class Menu (
             if (rol != "jugador" && rol != "entrenador") println("Esa opcion no es valida, intentalo de nuevo.")
         }while(rol != "jugador" && rol != "entrenador")
 
-        println("Escriba el nombre: ")
-        val nombre = readln()
+        return rol
+    }
 
-        println("Escriba los apellidos: ")
-        val apellidos = readln()
+    fun preguntarString (campo: String): String {
 
+        if (campo == "nombre") println("Escriba el nombre:")
+        if (campo == "apellidos") println("Escriba los apellidos:")
+        if (campo == "pais") println("Escriba el país:")
 
+        val input = readln()
+
+        return input
+    }
+
+    fun preguntarFechas (nacimiento: Boolean): LocalDate {
         val regex = """^\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$""".toRegex()
-        var fechaNacimiento = LocalDate.now()
+        var fechaFormato = LocalDate.now()
         var fecha: String
         do {
-            println("Escriba la fecha de nacimiento en formato yyyy-MM-dd Ej. 2000-01-01")
+            if (nacimiento) {
+                println("Escriba la fecha de nacimiento en formato yyyy-MM-dd Ej. 2000-01-01")
+            } else {
+                println("Escriba la fecha de incorporacion en formato yyyy-MM-dd Ej. 2000-01-01")
+            }
+
             fecha = readln()
             if (regex.matches(fecha)) {
                 try {
-                    fechaNacimiento = LocalDate.parse(fecha)
+                    fechaFormato = LocalDate.parse(fecha)
                 }
                 catch (e: Exception) {
                     fecha = "a"
@@ -92,125 +179,111 @@ class Menu (
             if(!regex.matches(fecha)) println("Fecha no valida.")
         }while (!regex.matches(fecha))
 
-        var fechaIncorporacion = LocalDate.now()
-        do {
-            println("Escriba la fecha de incorporacion en formato yyyy-MM-dd Ej. 2000-01-01")
-            fecha = readln()
-            if (regex.matches(fecha)) {
-                try {
-                    fechaIncorporacion = LocalDate.parse(fecha)
-                }
-                catch (e: Exception) {
-                    fecha = "a"
-                }
-            }
-            if(!regex.matches(fecha)) println("Fecha no valida.")
-        }while (!regex.matches(fecha))
+        return fechaFormato
+    }
 
-        var salario: Double?
+    fun preguntarDoble (campo: String): Double {
+        var input: Double
         do {
-            println("Escriba el salario separando los decimales con un punto")
-            salario = readln().toDoubleOrNull()
-            if (salario == null) println("Ese salario no es valido, introduzcalo de nuevo por favor.")
-        }while (salario == null)
+            if (campo == "salario") println("Escriba el salario separando los decimales con un punto")
+            if (campo == "altura") println("Escriba la altura (m) separando los decimales con un punto")
+            if (campo == "peso") println("Escriba el peso (kg) separando los decimales con un punto")
 
-        println("Escriba el pais de nacimiento: ")
-        val pais = readln()
+            input = readln().toDoubleOrNull() ?: -1.0
+            if (input == -1.0) println("Dato no valido, introduzcalo de nuevo por favor.")
+        }while (input == -1.0)
+
+        return input
+    }
+
+    fun preguntarPosicion(): Posicion{
+        var option: Int
+        do {
+            println("Escriba el numero indicado en la posicion de la que juega el jugador \n1| Centrocampista, 2| Delantero, 3| Portero, 4| Defensa: ")
+            option = readln().toIntOrNull() ?: 0
+            if (option !in 1..4) println("Opcion no valida.")
+        }while (option !in 1..4)
+        var posicion: Posicion
+        when(option){
+            1 -> posicion = Posicion.CENTROCAMPISTA
+            2 -> posicion = Posicion.DELANTERO
+            3 -> posicion = Posicion.PORTERO
+            else -> posicion = Posicion.DEFENSA
+        }
+        return posicion
+    }
+
+    fun preguntarEntero(campo: String): Int{
+        var input: Int?
+
+        if (campo == "dorsal") println("Escriba la dorsal del jugador")
+        if (campo == "goles") println("Escriba los goles del jugador")
+        if (campo == "partidos") println("Escriba el número de partidos del jugador")
+
+        do {
+            input = readln().toIntOrNull()
+            if (input == null) println("Dato no válido")
+        }while (input == null)
+
+        return input
+    }
+
+    fun preguntarEspecialidad (): Especialidad {
+        var option: Int
+        do {
+            println("Escriba el numero indicado en la especialidad del entrenador \n1| Principal, 2| Asistente, 3| Porteros")
+            option = readln().toIntOrNull() ?: 0
+            if (option !in 1..3) println("Opcion no valida.")
+        }while (option !in 1..3)
+        var especialidad: Especialidad
+        when(option){
+            1 -> especialidad = Especialidad.ENTRENADOR_PRINCIPAL
+            2 -> especialidad = Especialidad.ENTRENADOR_ASISTENTE
+            else -> especialidad = Especialidad.ENTRENADOR_PORTEROS
+        }
+        return especialidad
+    }
+
+    fun crearMiembro(){
+        var rol = preguntarRol()
 
         if(rol == "jugador"){
-            var option: Int
-            do {
-                println("Escriba el numero indicado en la posicion de la que juega el jugador \n1| Centrocampista, 2| Delantero, 3| Portero, 4| Defensa: ")
-                option = readln().toIntOrNull() ?: 0
-                if (option !in 1..4) println("Opcion no valida.")
-            }while (option !in 1..4)
-            var posicion: Posicion
-            when(option){
-                1 -> posicion = Posicion.CENTROCAMPISTA
-                2 -> posicion = Posicion.DELANTERO
-                3 -> posicion = Posicion.PORTERO
-                else -> posicion = Posicion.DEFENSA
-            }
-            var dorsal: Int?
-            do {
-                println("Escriba la dorsal del jugador")
-                dorsal = readln().toIntOrNull()
-                if (dorsal == null) println("Esa dorsal no es valida.")
-            }while (dorsal == null)
-
-            var altura: Double?
-            do {
-                println("Escriba la altura (m) separando los decimales con un punto")
-                altura = readln().toDoubleOrNull()
-                if (altura == null) println("Esa altura no es valida.")
-            }while (altura == null)
-
-            var peso: Double?
-            do {
-                println("Escriba el peso (kg) separando los decimales con un punto")
-                peso = readln().toDoubleOrNull()
-                if (peso == null) println("Ese peso no es valido.")
-            }while (peso == null)
-
-            var goles: Int?
-            do {
-                println("Escriba los goles del jugador")
-                goles = readln().toIntOrNull()
-                if (goles == null) println("Ese numero de goles no es valido.")
-            }while (goles == null)
-
-            var partidosJugados: Int?
-            do {
-                println("Escriba el numero de partidos del jugador")
-                partidosJugados = readln().toIntOrNull()
-                if (partidosJugados == null) println("Ese numero de partidos no es valido.")
-            }while (partidosJugados == null)
 
             val jugador = Jugador(
-                nombre = nombre,
-                apellidos = apellidos,
-                fecha_nacimiento = fechaNacimiento,
-                fecha_incorporacion = fechaIncorporacion,
-                salario = salario,
-                pais = pais,
-                posicion = posicion,
-                dorsal = dorsal,
-                altura = altura,
-                peso = peso,
-                goles = goles,
-                partidos_jugados = partidosJugados
+                nombre = preguntarString("nombre"),
+                apellidos = preguntarString("apellidos"),
+                fecha_nacimiento = preguntarFechas(true),
+                fecha_incorporacion = preguntarFechas(false),
+                salario = preguntarDoble("salario"),
+                pais = preguntarString("pais"),
+                posicion = preguntarPosicion(),
+                dorsal = preguntarEntero("dorsal"),
+                altura = preguntarDoble("altura"),
+                peso = preguntarDoble("peso"),
+                goles = preguntarEntero("goles"),
+                partidos_jugados = preguntarEntero("partidos")
             )
             try {
                 service.save(jugador)
+                println("Jugador guardado con éxito")
             }
             catch (e: Exception) {
                 println("Error al crear jugador: ${e.message}")
             }
-        }
-        else{
-            var option: Int
-            do {
-                println("Escriba el numero indicado en la especialidad del entrenador \n1| Principal, 2| Asistente, 3| Porteros")
-                option = readln().toIntOrNull() ?: 0
-                if (option !in 1..3) println("Opcion no valida.")
-            }while (option !in 1..3)
-            var especialidad: Especialidad
-            when(option){
-                1 -> especialidad = Especialidad.ENTRENADOR_PRINCIPAL
-                2 -> especialidad = Especialidad.ENTRENADOR_ASISTENTE
-                else -> especialidad = Especialidad.ENTRENADOR_PORTEROS
-            }
+        } else{
+
             val entrenador = Entrenador(
-                nombre = nombre,
-                apellidos = apellidos,
-                fecha_nacimiento = fechaNacimiento,
-                fecha_incorporacion = fechaIncorporacion,
-                salario = salario,
-                pais = pais,
-                especialidad = especialidad
+                nombre = preguntarString("nombre"),
+                apellidos = preguntarString("apellidos"),
+                fecha_nacimiento = preguntarFechas(true),
+                fecha_incorporacion = preguntarFechas(false),
+                salario = preguntarDoble("salario"),
+                pais = preguntarString("pais"),
+                especialidad = preguntarEspecialidad()
             )
             try {
                 service.save(entrenador)
+                println("Entrenador guardado con éxito")
             }
             catch (e: Exception) {
                 println("Error al crear entrenador: ${e.message}")
